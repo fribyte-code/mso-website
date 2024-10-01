7d66e42d9e078e6a2d79f7073db444fd1e66adbb
Johannes Skivdal <johannes@skivdaldata.no>
2024-10-01 15:57:01 +0200

    Initial commit
    - Generates a wagtail app
    - Basic gitignore
    - Poetry as the package manager

---
+++ mso_website/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-$t^@2)tnb5tal^!s0e=95+#@m$dr!e!qxagr7pai(5fm+p#qsn"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass
