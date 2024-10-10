4ec9be41a2660814be996ca7f4b9ef16b23f4d23
Kai Waløen <kai.waloen@gmail.com>
2024-10-10 19:02:07 +0200

    making headway with wagtail

--- home/models.py
+++ home/models.py
@@ -1,7 +1,35 @@
 from django.db import models
+from wagtail.admin.panels import MultiFieldPanel, FieldPanel
+from wagtail.fields import RichTextField
 
 from wagtail.models import Page
 
 
+from django.db import models
+
+from wagtail.models import Page
+from wagtail.fields import RichTextField
+
+# import MultiFieldPanel:
+from wagtail.admin.panels import FieldPanel, MultiFieldPanel
+
+
 class HomePage(Page):
-    pass
+
+    hero_text = models.CharField(
+        blank=True,
+        max_length=255, help_text="Write an introduction for the site"
+    )
+
+    body = RichTextField(blank=True)
+
+    # modify your content_panels:
+    content_panels = Page.content_panels + [
+        MultiFieldPanel(
+            [
+                FieldPanel("hero_text"),
+            ],
+            heading="Hero section",
+        ),
+        FieldPanel('body'),
+    ]
