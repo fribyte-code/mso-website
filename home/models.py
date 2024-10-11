bfdb107a1851acd9bffcd112764d9bb62d4b3860
Kai.Waloen <kawal5842@uib.no>
2024-10-11 17:06:08 +0200

    Removed unused imports

--- home/models.py
+++ home/models.py
@@ -1,28 +1,19 @@
-from django.db import models
-from wagtail.admin.panels import MultiFieldPanel, FieldPanel
-from wagtail.fields import RichTextField
-
-from wagtail.models import Page
-
 from wagtail.models import Page
 from wagtail.fields import RichTextField
-
-# import MultiFieldPanel:
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
 
 
 class DefaultPage(Page):
-
     # We use RichTextField for text fields
     # so that the client can format the text as they like.
 
-    #Hero_text is the subtitle of the page
+    # Hero_text is the subtitle of the page
     hero_text = RichTextField(
         blank=True,
         max_length=255, help_text="Write an introduction for the site"
     )
 
-    #This is the body of the text
+    # This is the body of the text
     body = RichTextField(blank=True, help_text="Write a body for your site")
 
     # modify your content_panels:
