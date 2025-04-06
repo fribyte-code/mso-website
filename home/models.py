67f76113c8de879d97b7c1851c786f96cb4b5d0f
KWaloen <kai.waloen@gmail.com>
2025-04-06 14:40:26 +0200

    starting point for Bestilling form

--- home/models.py
+++ home/models.py
@@ -1,7 +1,13 @@
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import FieldPanel, MultiFieldPanel
-
+from django.db import models
+from modelcluster.fields import ParentalKey
+from wagtail.admin.panels import (
+    FieldPanel, FieldRowPanel,
+    InlinePanel, MultiFieldPanel
+)
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
 
 class DefaultPage(Page):
 
@@ -26,4 +32,25 @@ class DefaultPage(Page):
             heading="Hero section",
         ),
         FieldPanel('body'),
+    ]
+
+class FormField(AbstractFormField):
+    page = ParentalKey('FormPage', on_delete=models.CASCADE, related_name='form_fields')
+
+
+class FormPage(AbstractEmailForm):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel('intro'),
+        InlinePanel('form_fields', label="Form fields"),
+        FieldPanel('thank_you_text'),
+        MultiFieldPanel([
+            FieldRowPanel([
+                FieldPanel('from_address', classname="col6"),
+                FieldPanel('to_address', classname="col6"),
+            ]),
+            FieldPanel('subject'),
+        ], "Email"),
     ]
