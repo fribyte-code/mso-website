c86092f1b2839b9b8aabe0cb50f59955af22797c
KWaloen <kai.waloen@gmail.com>
2025-05-07 19:19:30 +0200

    put login url back in views.py

--- management/views.py
+++ management/views.py
@@ -2,7 +2,7 @@ from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 from django.contrib.auth import logout
 
-@login_required(redirect_field_name="next")
+@login_required(redirect_field_name="next", login_url="/management/login/")
 def index(request):
     return render(request, "management.html")
 
