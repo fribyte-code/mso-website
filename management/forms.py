9a605a107a3a6191780d6c73c609079ed92e2228
KWaloen <kai.waloen@gmail.com>
2025-05-09 13:13:44 +0200

    limit the amount of fields the user can change

--- management/forms.py
+++ management/forms.py
@@ -4,5 +4,10 @@ from .models import Profile
 class ProfileForm(forms.ModelForm):
     class Meta: 
         model = Profile
+        fields = ["telefon", "kjønn", "kull"]
+
+class AdminProfileForm(forms.ModelForm):
+    class Meta(ProfileForm.Meta): 
+        model = Profile
         fields = ["telefon", "kjønn", "kull", "erfaren", "pu_erfaren", "fus_erfaren", "timer", "internundervisning", "styremedlem"]
         
